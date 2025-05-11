f9df81f9d520b8e3a20f570ca7822c1f5f424845
fstanew <fstanew@gmail.com>
2025-05-11 16:39:56 +0200

    zapis trafień do matchex.txt

--- main.c
+++ main.c
@@ -8,8 +8,7 @@
 
 #define PROGRESS_BAR_WIDTH 50
 
-static char *strcasestr_local(const char *h, const char *n)
-{
+static char *strcasestr_local(const char *h, const char *n) {
     if (!*n) return (char *)h;
     size_t nlen = strlen(n);
     for (const char *p = h; *p; ++p)
@@ -18,12 +17,10 @@ static char *strcasestr_local(const char *h, const char *n)
     return NULL;
 }
 
-static void update_progress_bar(int downloaded, int total)
-{
+static void update_progress_bar(int downloaded, int total) {
     if (total <= 0) return;
     float fraction = (float)downloaded / total;
-    int filled   = (int)(fraction * PROGRESS_BAR_WIDTH);
-
+    int filled = (int)(fraction * PROGRESS_BAR_WIDTH);
     printf("\r[");
     for (int i = 0; i < PROGRESS_BAR_WIDTH; ++i)
         putchar(i < filled ? '#' : ' ');
@@ -31,35 +28,26 @@ static void update_progress_bar(int downloaded, int total)
     fflush(stdout);
 }
 
-int main(void)
-{
+int main(void) {
     WSADATA d;
-    if (WSAStartup(0x202, &d) != 0) {
-        printf("Blad!");
-        return 1;
-    }
+    if (WSAStartup(0x202, &d) != 0) return 1;
 
     SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (s == INVALID_SOCKET) {
-        printf("Nie dziala socket\n");
         WSACleanup();
         return 2;
     }
 
     char host[100], sciezka[200], slowo[50];
-
     printf("Podaj hosta (np. google.com): ");
     scanf("%99s", host);
-
     printf("Podaj sciezke (np. / lub /search): ");
     scanf("%199s", sciezka);
-
     printf("Podaj slowo do zliczenia w odpowiedzi (np. html): ");
     scanf("%49s", slowo);
 
     struct hostent *h = gethostbyname(host);
     if (!h) {
-        printf("Nie znaleziono hosta\n");
         closesocket(s);
         WSACleanup();
         return 3;
@@ -67,55 +55,55 @@ int main(void)
 
     struct sockaddr_in serwer = {0};
     serwer.sin_family = AF_INET;
-    serwer.sin_port   = htons(80);
-    serwer.sin_addr   = *(struct in_addr *)h->h_addr;
+    serwer.sin_port = htons(80);
+    serwer.sin_addr = *(struct in_addr *)h->h_addr;
 
-    if (connect(s,(struct sockaddr *)&serwer,sizeof(serwer)) == SOCKET_ERROR) {
-        printf("Nie polaczono z serwerem: %d\n", WSAGetLastError());
+    if (connect(s, (struct sockaddr *)&serwer, sizeof(serwer)) == SOCKET_ERROR) {
         closesocket(s);
         WSACleanup();
         return 4;
     }
-    printf("Poloczono z %s\n", host);
 
     DWORD startTime = GetTickCount();
 
     char request[512];
-    snprintf(request,sizeof(request),
+    snprintf(request, sizeof(request),
              "GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",
              sciezka, host);
 
-    if (send(s,request,strlen(request),0) == SOCKET_ERROR) {
-        printf("Blad wysylania\n");
+    if (send(s, request, strlen(request), 0) == SOCKET_ERROR) {
         closesocket(s);
         WSACleanup();
         return 5;
     }
 
-    FILE *plik = fopen("response.txt","w");
+    FILE *plik = fopen("response.txt", "w");
     if (!plik) {
-        printf("Nie mozna otworzyc pliku do zapisu!\n");
         closesocket(s);
         WSACleanup();
         return 6;
     }
 
-    char  buffer[2048];
-    int   odebrano,
-          totalBytes     = 0,
-          licznikWystap  = 0,
-          headerDone     = 0,
-          contentLength  = -1,
-          bodyBytes      = 0;
-    char  headerBuf[8192] = {0};
-    int   headerLen      = 0;
+    FILE *matches = fopen("matches.txt", "w");
+    if (!matches) {
+        fclose(plik);
+        closesocket(s);
+        WSACleanup();
+        return 7;
+    }
+
+    char buffer[2048];
+    int odebrano, totalBytes = 0, licznikWystap = 0, headerDone = 0;
+    int contentLength = -1, bodyBytes = 0;
+    char headerBuf[8192] = {0};
+    int headerLen = 0;
 
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     printf("Odpowiedz serwera:\n");
 
-    while ((odebrano = recv(s,buffer,sizeof(buffer)-1,0)) > 0) {
+    while ((odebrano = recv(s, buffer, sizeof(buffer) - 1, 0)) > 0) {
         buffer[odebrano] = '\0';
-        totalBytes      += odebrano;
+        totalBytes += odebrano;
 
         if (!headerDone) {
             if (headerLen + odebrano < (int)sizeof(headerBuf) - 1) {
@@ -132,44 +120,57 @@ int main(void)
             char *cl = strcasestr_local(headerBuf, "Content-Length:");
             if (cl) contentLength = atoi(cl + 15);
 
-            fwrite(headerBuf,1,(headerEnd + 4) - headerBuf,plik);
+            fwrite(headerBuf, 1, (headerEnd + 4) - headerBuf, plik);
 
             int headerBytesThisChunk = (headerEnd + 4) - (headerBuf + headerLen - odebrano);
             int bodyOffset = odebrano - headerBytesThisChunk;
             if (bodyOffset > 0) {
                 char *ptr = buffer + headerBytesThisChunk;
                 bodyBytes += bodyOffset;
-                fwrite(ptr,1,bodyOffset,plik);
+                fwrite(ptr, 1, bodyOffset, plik);
 
                 while (*ptr && ptr < buffer + odebrano) {
                     char *found = strstr(ptr, slowo);
                     if (found && found < buffer + odebrano) {
-                        fwrite(ptr,1,found - ptr,stdout);
+                        fwrite(ptr, 1, found - ptr, stdout);
                         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
-                        fwrite(found,1,strlen(slowo),stdout);
+                        fwrite(found, 1, strlen(slowo), stdout);
                         SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
                         licznikWystap++;
+
+                        char *line_start = found;
+                        while (line_start > buffer && *(line_start - 1) != '\n' && *(line_start - 1) != '\r') line_start--;
+                        char *line_end = found;
+                        while (line_end < buffer + odebrano && *line_end != '\n') line_end++;
+                        fprintf(matches, "%.*s\n", (int)(line_end - line_start), line_start);
+
                         ptr = found + strlen(slowo);
                     } else {
-                        fwrite(ptr,1,(buffer + odebrano) - ptr,stdout);
+                        fwrite(ptr, 1, (buffer + odebrano) - ptr, stdout);
                         break;
                     }
                 }
             }
-        }
-        else {
+        } else {
             bodyBytes += odebrano;
-            fwrite(buffer,1,odebrano,plik);
+            fwrite(buffer, 1, odebrano, plik);
 
             char *ptr = buffer;
             while (*ptr) {
                 char *found = strstr(ptr, slowo);
                 if (found) {
-                    fwrite(ptr,1,found - ptr,stdout);
+                    fwrite(ptr, 1, found - ptr, stdout);
                     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
-                    fwrite(found,1,strlen(slowo),stdout);
+                    fwrite(found, 1, strlen(slowo), stdout);
                     SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
                     licznikWystap++;
+
+                    char *line_start = found;
+                    while (line_start > buffer && *(line_start - 1) != '\n' && *(line_start - 1) != '\r') line_start--;
+                    char *line_end = found;
+                    while (line_end < buffer + odebrano && *line_end != '\n') line_end++;
+                    fprintf(matches, "%.*s\n", (int)(line_end - line_start), line_start);
+
                     ptr = found + strlen(slowo);
                 } else {
                     printf("%s", ptr);
@@ -185,6 +186,7 @@ int main(void)
     if (contentLength > 0) putchar('\n');
 
     fclose(plik);
+    fclose(matches);
 
     DWORD endTime = GetTickCount();
     printf("\n\nLacznie odebrano %d bajtow danych.\n", totalBytes);
